772fa127d9b333e022730756e306b1de14c80f21
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2026-01-21 13:20:49 +0530

    patch copywrite

--- src/lib/components/FleetSection.svelte
+++ src/lib/components/FleetSection.svelte
@@ -6,7 +6,7 @@
 
 <section class="py-20 bg-white">
 	<div class="container mx-auto px-6">
-		<h2 class="text-5xl md:text-5xl text-center text-blue-600 mb-12 uppercase tracking-wide">Around the Farm</h2>
+		<h2 class="text-3xl md:text-3xl text-center text-blue-600 mb-12 uppercase tracking-wide">Rides Start From Rs 500/- to 1200/- Based on ATV Size, Solo/Double Rides For Duration of 15 minutes or 5 Laps Around The Farm</h2>
 
 		<div class="grid grid-cols-1 md:grid-cols-3 gap-10">
 			<!-- 300cc ATV (Red) -->
@@ -29,19 +29,19 @@
 				</div>
 			</div>
 
-			<!-- 300cc ATV (Blue) -->
+			<!-- 135cc ATV (Blue) -->
 			<div class="bg-white rounded-2xl shadow-xl overflow-hidden flex flex-col">
 				<div class="aspect-square overflow-hidden rounded-t-2xl">
 					<img
 						src={atvBlue}
-						alt="300cc Blue ATV"
+						alt="135cc  Blue ATV"
 						class="size-full object-cover object-center"
 					/>
 				</div>
 				<div class="p-6">
-					<h3 class="text-3xl mb-4">300cc ATV</h3>
+					<h3 class="text-3xl mb-4">135cc ATV</h3>
 					<p class="text-lg">
-						High-performance ATV suitable for single or double riders.
+						Suitable for teenagers seeking a thrilling ride.
 						<strong class="block mt-4">Rates:</strong>
 						₹800 — Single rider<br />
 						₹1200 — Double riders
