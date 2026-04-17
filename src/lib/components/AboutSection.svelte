52e3f3aec4c0adfc668018ff1042ef3b301683e3
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2026-04-17 15:33:13 +0530

    add farm image

--- src/lib/components/AboutSection.svelte
+++ src/lib/components/AboutSection.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import farm from '$lib/assets/taajem-farm.jpeg';
+</script>
+
 <section class="py-20 bg-white">
 	<div class="container mx-auto px-6">
 		<h2 class="text-4xl md:text-5xl text-center text-blue-600 mb-12 uppercase tracking-wide">About The ATV Club</h2>
@@ -9,9 +13,9 @@
 				<p>All rides include safety gear and briefing. Reach out directly for availability and personalized quotes based on group size and duration.</p>
 			</div>
 			<img
-				src="https://images.unsplash.com/photo-1473448912268-2022ce9509d8?auto=format&fit=crop&q=80&w=2940"
+				src={farm}
 				alt="Scenic trails in Goa"
-				class="rounded-2xl shadow-2xl object-cover w-full h-full"
+				class="rounded-2xl shadow-2xl object-cover w-full h-170"
 			/>
 		</div>
 	</div>
