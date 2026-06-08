00c1a83752683c8e4650b1fb5c102407d0e183a7
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2026-06-08 20:46:51 +0530

    updated pricing

--- src/lib/components/FleetSection.svelte
+++ src/lib/components/FleetSection.svelte
@@ -6,7 +6,7 @@
 
 <section class="py-20 bg-white">
 	<div class="container mx-auto px-6">
-		<h2 class="text-3xl md:text-3xl text-center text-blue-600 mb-12 uppercase tracking-wide">Rides Start From Rs 600/- to 1400/- Based on ATV Size, Solo/Double Rides For Duration of 25-30 minutes or 6 Rounds (approx 2.5 km) Around The Farm</h2>
+		<h2 class="text-3xl md:text-3xl text-center text-blue-600 mb-12 uppercase tracking-wide">Rides Start From Rs 700/- to 1600/- Based on ATV Size, Solo/Double Rides For Duration of 25-30 minutes or 6 Rounds (approx 2.5 km) Around The Farm</h2>
 
 		<div class="grid grid-cols-1 md:grid-cols-3 gap-10">
 			<!-- Big ATV (Red) -->
@@ -23,8 +23,8 @@
 					<p class="text-lg">
 						High-performance ATV suitable for single or double riders.
 						<strong class="block mt-4">Rates:</strong>
-						₹900 — Single rider 👤<br />
-						₹1400 — Double riders 👤👤
+						₹1100 — Single rider 👤<br />
+						₹1600 — Double riders 👤👤
 					</p>
 				</div>
 			</div>
@@ -43,7 +43,7 @@
 					<p class="text-lg">
 						Suitable for teenagers seeking a thrilling ride.
 						<strong class="block mt-4">Rates:</strong>
-						₹700 — Single rider 👤<br />
+						₹900 — Single rider 👤<br />
 					</p>
 				</div>
 			</div>
@@ -62,7 +62,7 @@
 					<p class="text-lg">
 						Perfect for first-timers, younger riders or those wanting a gentler experience.
 						<strong class="block mt-4">Rates:</strong>
-						₹600 — Single rider 👤
+						₹700 — Single rider 👤
 					</p>
 				</div>
 			</div>
